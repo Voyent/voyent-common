00f1ab3177e104f43c7a0f8bb5a3b8613afc95b1
Deryk Sinotte <watnotte@gmail.com>
2015-03-02 11:25:50 -0800

    add grunt file for easily bumping version

---
+++ gruntfile.js
@@ -0,0 +1,43 @@
+/*
+ * ICESOFT COMMERCIAL SOURCE CODE LICENSE V 1.1
+ *
+ * The contents of this file are subject to the ICEsoft Commercial Source
+ * Code License Agreement V1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the
+ * License at
+ * http://www.icesoft.com/license/commercial-source-v1.1.html
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * Copyright 2009-2014 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ */
+
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-bump');
+
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                regExp: false
+            }
+        }
+    });
+};
+
