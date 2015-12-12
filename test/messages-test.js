3f02abf5962f01d22e86b442774443551f7ff931
cruzmiraback <cmiraback@icesoft.com>
2015-12-11 17:26:03 -0700

    NTFY-316 - Change message topic URL root.

--- test/messages-test.js
+++ test/messages-test.js
@@ -5,7 +5,7 @@ describe('Messages', function () {
 
     it('publish', function (done) {
         assert(messages);
-        messages.publish('services/test/publishMessage', 'Test message @ ' + new Date().toISOString());
+        messages.publish('events/test/publishMessage', 'Test message @ ' + new Date().toISOString());
         setTimeout(function () {
             done();
         }, 1000);
