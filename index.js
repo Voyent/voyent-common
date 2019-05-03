e069150f9a76009aed75a817097715a9bc2b03b9
Deryk Sinotte <watnotte@users.noreply.github.com>
2019-05-03 11:09:38 -0700

    VRAS-489: update JSDoc and ensure callback is called if one is provided

--- index.js
+++ index.js
@@ -27,52 +27,63 @@ function getExports() {
 }
 
 /**
- * Returns a set of the exports for the entire module.  If an optional callback
- * is provided, the exports are returned asynchronously.
+ * Returns a set of the exports for the entire module.
+ *
+ * If, optionally, a set of keys and a callback are provided, the exports are
+ * returned asynchronously.
  *
  * This library is used by all of our Node-based services.  It used to simply
- * return all the exported sub-modules but we needed to change that to support
- * getting and setting the environment variables from the S3 bucket.
+ * return all the exported sub-modules.  However we needed to change the behaviour
+ * to support getting and setting the environment variables from the S3 bucket.
  *
  * The main issue is that the S3 bucket contains configuration files that we
- * want to set as environment variables that are available to all other service
- * code.  However, it's important that these variables are set first, before
- * any other code is run.  This is of particular importance for the db connection
- * string that is stored as DB_URI in common.config.
+ * want to set as environment variables which are made available to all of the
+ * service code.  Therefore, it's important that these variables are set first,
+ * before any other code is executed.  This is of particular importance for things
+ * like the db connection string that is stored as DB_URI in common.config.
  *
- * So the module now returns a function.  Instead of calling:
+ * So the module provides a function as the default return value.  When requiring
+ * voyent-common, instead of calling:
  *
  * const vc = require('voyent-common');
  * const logger = vc.loggers.getLogger();
  *
- * You should now call "require" as a function. In most places, the usage
- * barely changes:
+ * You should now call "require" and then execute the returned value as a
+ * function. In most places, the usage barely changes:
  *
  * const vc = require('voyent-common')();
  * const logger = vc.loggers.getLogger();
  *
- * The function can optionally take a callback.  The callback only needs
- * to be used to ensure the environment variables are set before the sub-modules
- * are returned.  This is generally only done in the start.js files of our
- * services.  In those cases, the value that is returned as a result of the
+ * For the special case of the service startup, the function can optionally take
+ * an array of filename keys and a callback.  The array will look something like:
+ *
+ * ['common.config', 'action-service-node.config']
+ *
+ * In general, the keys and callback only need to be used during service startup
+ * to ensure the environment variables are set before the sub-modules are
+ * evaluated and returned. In those cases, the value that is returned as a result of the
  * async callback can then be used as normal:
  *
- * require('voyent-common')( function(err, vc){
+ * require('voyent-common')( configFilenames, function(err, vc){
  *     const logger = vc.loggers.getLogger();
  * }
  *
- * @param cb (optional)
- * @returns {*} The exported sub modules.
+ * @param keys (optional) Array of S3 bucket keys
+ * @param cb (optional) Function to execute after the S3 bucket values have been fetched
+ *           and set as environment variables.
+ * @returns {*} The exported sub modules.  These are always returned whether or
+ *              not there was an error.
  */
 module.exports = function (keys, cb) {
 
     if(!keys || !cb){
-        console.log('voyent-common/index.js: missing param(s)', keys, cb);
-        return getExports();
+        if(!cb){
+            return getExports();
+        }
+        cb(null, getExports());
     }
 
     if (environmentProperties) {
-        console.log('voyent-common/index.js: environment properties already set');
         return cb(null, getExports());
     }
 
